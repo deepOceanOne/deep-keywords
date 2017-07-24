851f0a0174f76042426f94e805f37deec576d57a
lifeinbofyx@sina.com <lifeinbofyx@sina.com>
2017-07-24 21:31:53 +0800

    normal update
    see if proxy works.

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var proxy = require('http-proxy-middleware');
  
 var app = express();
  
-app.use('/', proxy({target: 'http://www.baidu.com', changeOrigin: true}));
+app.use('/', proxy({target: 'http://www.sina.com.cn', changeOrigin: true}));
 app.listen(22);
